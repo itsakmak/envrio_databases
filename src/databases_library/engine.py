396693778660e63b9d32e41fa01830dd3953d96d
itsakmak <xylopodaros@yahoo.gr>
2023-11-21 23:00:28 +0200

    Removing Localsession

--- src/databases_library/engine.py
+++ src/databases_library/engine.py
@@ -3,16 +3,12 @@ __author__='Ioannis Tsakmakis'
 __date_created__='2023-11-21'
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 import json
 
 with open('mysql_config.json','r') as f:
     config = json.load(f)
 
 engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}', echo=True)
-
-LocalSession = sessionmaker(bind=engine,expire_on_commit=True)
-
 class Base(DeclarativeBase):
     pass
-
