6b42e0272b3fa90d19c693f4ec5eb60cd29ee433
itsakmak <xylopodaros@yahoo.gr>
2023-11-21 20:50:15 +0200

    minor updates

--- src/modules/engine.py
+++ src/modules/engine.py
@@ -1,11 +1,15 @@
-__version__='1.0.0'
+__version__='1.0.1'
 __author__='Ioannis Tsakmakis'
-__date_created__='2023-10-31'
+__date_created__='2023-11-21'
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+import json
 
-engine = create_engine("mysql+mysqlconnector://username:password@host-ip/database", echo=True)
+with open('mysql_config.json','r') as f:
+    config = json.load(f)
+
+engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}', echo=True)
 
 LocalSession = sessionmaker(bind=engine,expire_on_commit=True)
 
