dfdfb1d82dd94d116df03cf297d38a02d0dbc1a1
itsakmak <xylopodaros@yahoo.gr>
2023-11-22 09:18:13 +0200

    adding the pool_pre_ping to engine

--- src/databases_library/engine.py
+++ src/databases_library/engine.py
@@ -1,6 +1,6 @@
-__version__='1.0.1'
+__version__='1.0.2'
 __author__='Ioannis Tsakmakis'
-__date_created__='2023-11-21'
+__date_created__='2023-11-22'
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
@@ -9,6 +9,7 @@ import json
 with open('mysql_config.json','r') as f:
     config = json.load(f)
 
-engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}', echo=True)
+engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}', 
+                       pool_pre_ping=True)
 class Base(DeclarativeBase):
     pass
