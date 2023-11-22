2dbe10f116a5f65d065a981cccbb8993cb2fc3c5
itsakmak <xylopodaros@yahoo.gr>
2023-11-22 09:30:38 +0200

    fixing config file paths

--- src/databases_library/engine.py
+++ src/databases_library/engine.py
@@ -4,9 +4,13 @@ __date_created__='2023-11-22'
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
-import json
+import json, os
 
-with open('mysql_config.json','r') as f:
+local_directory = os.path.dirname(os.path.abspath(__file__))
+
+config_path = os.path.join(local_directory, 'mysql_config.json')
+
+with open(config_path,'r') as f:
     config = json.load(f)
 
 engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}', 
