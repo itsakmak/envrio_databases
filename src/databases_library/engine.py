477ef52b8d75f35d1968028615750e9e86fa57d2
itsakmak <xylopodaros@yahoo.gr>
2023-11-23 18:43:30 +0200

    Solving connection issues

--- src/databases_library/engine.py
+++ src/databases_library/engine.py
@@ -4,16 +4,17 @@ __date_created__='2023-11-22'
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
-import json, os
+import os, json
 
-local_directory = os.path.dirname(os.path.abspath(__file__))
+def create_db_engine():
+    local_directory = os.path.dirname(os.path.abspath(__file__))
 
-config_path = os.path.join(local_directory, 'mysql_config.json')
+    config_path = os.path.join(local_directory, 'mysql_config.json')
 
-with open(config_path,'r') as f:
-    config = json.load(f)
+    with open(config_path,'r') as f:
+        config = json.load(f)
 
-engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}', 
+    return create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}', 
                        pool_pre_ping=True)
 class Base(DeclarativeBase):
     pass
