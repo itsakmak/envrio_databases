b72913d9fee74986fa273da76d2db62df6c28263
itsakmak <xylopodaros@yahoo.gr>
2023-11-28 11:39:18 +0200

    switch to Session.begin()

--- src/databases_library/crud.py
+++ src/databases_library/crud.py
@@ -1,119 +1,106 @@
-__version__='1.0.4'
+__version__='1.0.5'
 __author__='Ioannis Tsakmakis'
 __date_created__='2023-10-20'
-__last_updated__='2023-11-27'
+__last_updated__='2023-11-28'
 
 import databases_library.schemas as schemas
 import databases_library.models as models
-from databases_library.engine import engine
-from sqlalchemy.orm import sessionmaker, Session
+from databases_library.engine import Session
 from sqlalchemy import text, or_
 
 # User Management
 
 class User:
-    
-    @staticmethod
-    def add(user: schemas.UsersTableCreate, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
-        new_user = models.Users(name=user.name, email=user.email, subscription_expires_in=user.subscription_expires_in)
-        db.add(new_user)
-        db.commit()
 
     @staticmethod
-    def get_by_name(name: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
-        return db.query(models.Users).filter_by(name=name).first()
-    
+    def add(user: schemas.UsersTableCreate, db: Session.begin()):
+        new_user = models.Users(name=user.name, email=user.email, subscription_expires_in=user.subscription_expires_in)
+        db.add(new_user)          
+
     @staticmethod
-    def get_by_id(id: int, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_name(name: str, db: Session.begin()):
+        return db.query(models.Users).filter_by(name=name).first()
+
+    @staticmethod
+    def get_by_id(id: int, db: Session.begin()):
         return db.query(models.Users).filter_by(id=id).first()
     
     @staticmethod
-    def get_by_email(email: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
-        return db.query(models.Users).filter_by(email=email).first()
-    
+    def get_by_email(email: str, db: Session.begin()):
+            return db.query(models.Users).filter_by(email=email).first()
 class Stations:
 
     @staticmethod
-    def add(station: schemas.StationsCreate, db: Session=sessionmaker(bind=engine,expire_on_commit=True)()):
+    def add(station: schemas.StationsCreate, db: Session.begin()):
         new_station = models.Stations(brand=station.brand, model=station.model, code=station.code, date_created=station.date_created,
                                       longitude=station.longitude, latitude=station.latitude, elevation=station.elevation,
                                       access=station.access, name=station.name, icon_type=station.icon_type)
         db.add(new_station)
-        db.commit()
 
     @staticmethod
-    def get_by_code(code: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_code(code: str, db: Session=Session.begin()):
         return db.query(models.Stations).filter_by(code = code).first()
-    
+
     @staticmethod
-    def get_by_brand(brand: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_brand(brand: str, db: Session=Session.begin()):
         return db.query(models.Stations).filter_by(brand = brand).all()
     
     @staticmethod
-    def get_by_access(user_id: int, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
-        result = db.query(models.Stations).filter(text("JSON_CONTAINS(JSON_UNQUOTE(JSON_EXTRACT(access, '$.users')), CAST(:user AS JSON), '$')").params(user=user_id)).all()
-        return result
+    def get_by_access(user_id: int, db: Session=Session.begin()):
+        return db.query(models.Stations).filter(text("JSON_CONTAINS(JSON_UNQUOTE(JSON_EXTRACT(access, '$.users')), CAST(:user AS JSON), '$')").params(user=user_id)).all()
     
     @staticmethod
-    def update_date_created(station_id: int, new_datetime: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def update_date_created(station_id: int, new_datetime: str, db: Session.begin()):
         station=db.query(models.Stations).filter_by(id=station_id).first()
         if station is not None:
             station.date_created=new_datetime
-            db.commit()
         else:
             db.close()
-    
+
     @staticmethod
-    def delete_by_code(code: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def delete_by_code(code: str, db: Session.begin()):
         result = db.query(models.Stations).filter_by(code = code).first()
         if result is not None:
             db.delete(result)
-            db.commit()
         else: db.close()
-
-    
 class Gateways:
 
     @staticmethod
-    def add(gateway: schemas.GatewaysCreate, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def add(gateway: schemas.GatewaysCreate, db: Session=Session.begin()):
         new_gateway = models.GateWays(brand= gateway.brand, model=gateway.model, code=gateway.code,
                                       name = gateway.name, station_id=gateway.station_id)
         db.add(new_gateway)
-        db.commit()
 
     @staticmethod
-    def get_by_code(code: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_code(code: str, db: Session.begin()):
         return db.query(models.GateWays).filter_by(code=code).first()
-    
 class RemoteTerminalUnits:
 
     @staticmethod
-    def add(rtu: schemas.RemoteTerminalUnitsCreate, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def add(rtu: schemas.RemoteTerminalUnitsCreate, db: Session.begin()):
         new_rtu = models.RemoteTerminalUnits(brand=rtu.brand, model=rtu.model, code=rtu.code, longitude=rtu.longitude,
                                              latitude=rtu.latitude, elevation=rtu.elevation,name=rtu.name, station_id=rtu.station_id)
         db.add(new_rtu)
-        db.commit()
 
     @staticmethod
-    def get_by_code(code: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_code(code: str, db: Session=Session.begin()):
         return db.query(models.RemoteTerminalUnits).filter_by(code = code).first()
     
     @staticmethod
-    def get_by_station_id(station_id: int, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_station_id(station_id: int, db: Session=Session.begin()):
         return db.query(models.RemoteTerminalUnits).filter_by(station_id=station_id).first()
     
 class MonitoredParameters:
 
     @staticmethod
-    def add(monitored_parameters: schemas.MonitoredParametersCreate, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def add(monitored_parameters: schemas.MonitoredParametersCreate, db: Session=Session.begin()):
         new_monitored_parameters = models.MonitoredParameters(type=monitored_parameters.device_type, measurement=monitored_parameters.measurement, unit=monitored_parameters.unit,
                                                 device_height=monitored_parameters.device_height, name =monitored_parameters.name,
                                                 code=monitored_parameters.code, station_id=monitored_parameters.station_id, rtu_id=monitored_parameters.rtu_id)
         db.add(new_monitored_parameters)
-        db.commit()
 
     @staticmethod
-    def get_by_station_id(station_id: int, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_station_id(station_id: int, db: Session=Session.begin()):
         rtus = db.query(models.RemoteTerminalUnits).filter_by(station_id=station_id).first()
         if rtus is None:
             return db.query(models.MonitoredParameters).filter_by(station_id=station_id).all()
@@ -122,27 +109,26 @@ class MonitoredParameters:
                                                             models.MonitoredParameters.rtu_id.in_(rtus.id))).all()
     
     @staticmethod
-    def get_by_rtu_id(rtu_id: int, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_rtu_id(rtu_id: int, db: Session=Session.begin()):
         return db.query(models.MonitoredParameters).filter_by(rtu_id=rtu_id).all()
     
     @staticmethod
-    def get_by_station_id_and_rtu_id(station_id: int, rtu_id: int,db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_station_id_and_rtu_id(station_id: int, rtu_id: int,db: Session=Session.begin()):
         return db.query(models.MonitoredParameters).filter_by(station_id=station_id,rtu_id=rtu_id).all()
     
     @staticmethod
-    def get_by_id(id: int, db: Session = sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_by_id(id: int, db: Session = Session.begin()):
         return db.query(models.MonitoredParameters).filter_by(id = id).first()
 
 class MeasurementsTranslations:
 
     @staticmethod
-    def add(translation: schemas.MeasurementTranslationsCreate, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def add(translation: schemas.MeasurementTranslationsCreate, db: Session=Session.begin()):
         new_translation = models.MeasurementTranslations(measurement=translation.measurement, el=translation.el, en=translation.en)
         db.add(new_translation)
-        db.commit()
     
     @staticmethod
-    def get_translation_by_measurement(measurement: str, db: Session=sessionmaker(bind=engine, expire_on_commit=True)()):
+    def get_translation_by_measurement(measurement: str, db: Session=Session.begin()):
         return db.query(models.MeasurementTranslations).filter_by(measurement=measurement).first()
 
 
--- src/databases_library/engine.py
+++ src/databases_library/engine.py
@@ -1,11 +1,10 @@
-__version__='1.0.5'
+__version__='1.0.6'
 __author__='Ioannis Tsakmakis'
 __date_created__='2023-10-20'
-__last_updated__='2023-11-27'
+__last_updated__='2023-11-28'
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 import os, json
 
 local_directory = os.path.dirname(os.path.abspath(__file__))
@@ -15,20 +14,10 @@ config_path = os.path.join(local_directory, 'mysql_config.json')
 with open(config_path,'r') as f:
     config = json.load(f)
 
-ini_engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}',
+engine = create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}',
                          pool_pre_ping=True)
 
-def db_engine(engine,config):
-    try:
-        connection_check = engine.connect()
-        if connection_check:
-            return engine
-        else:
-            return create_engine(url=f'{config["DBAPI"]}://{config["username"]}:{config['password']}@{config["host-ip"]}/{config["database"]}',
-                         pool_pre_ping=True)
-    except OperationalError as e:
-        return {'code':'Database connection issue','message':e}
+Session = sessionmaker(engine)
 
-engine = db_engine(engine=ini_engine, config=config)
 class Base(DeclarativeBase):
     pass
