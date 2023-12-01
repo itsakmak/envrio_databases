4e58914fc40845f6b046211ae9d392a4c95487c1
itsakmak <xylopodaros@yahoo.gr>
2023-12-01 11:48:23 +0200

    modifing the logging report format

--- src/databases_library/crud.py
+++ src/databases_library/crud.py
@@ -1,17 +1,18 @@
-__version__='1.0.7'
+__version__='1.0.8'
 __authors__=['Ioannis Tsakmakis']
 __date_created__='2023-10-20'
-__last_updated__='2023-11-30'
+__last_updated__='2023-12-01'
 
 import databases_library.schemas as schemas
 import databases_library.models as models
 from databases_library.engine import SessionLocal, logging_path
 from sqlalchemy.orm import Session
 from sqlalchemy import text, or_, select, event
+from datetime import datetime
 import logging
 
 # Configure the logger
-logging.basicConfig(filename=f'{logging_path}/sqlalchemy_connection.log', level=logging.DEBUG)
+logging.basicConfig(filename=f'{logging_path}/sqlalchemy_connection.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('sqlalchemy')
 
 # Function to log SQLAlchemy session events
@@ -23,25 +24,30 @@ class User:
 
     @staticmethod
     def add(user: schemas.UsersTableCreate, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         new_user = models.Users(name=user.name, email=user.email, subscription_expires_in=user.subscription_expires_in)
         db.add(new_user)          
 
     @staticmethod
     def get_by_name(name: str, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         return db.execute(select(models.Users).filter_by(name=name)).first()
 
     @staticmethod
     def get_by_id(id: int, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         return db.execute(select(models.Users).filter_by(id=id)).first()
     
     @staticmethod
     def get_by_email(email: str, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         return db.execute(select(models.Users).filter_by(email=email)).first()
 
 class Stations:
 
     @staticmethod
     def add(station: schemas.StationsCreate, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         new_station = models.Stations(brand=station.brand, model=station.model, code=station.code, date_created=station.date_created,
                                       longitude=station.longitude, latitude=station.latitude, elevation=station.elevation,
                                       access=station.access, name=station.name, icon_type=station.icon_type)
@@ -82,32 +88,38 @@ class Gateways:
 
     @staticmethod
     def add(gateway: schemas.GatewaysCreate, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         new_gateway = models.GateWays(brand= gateway.brand, model=gateway.model, code=gateway.code,
                                       name = gateway.name, station_id=gateway.station_id)
         db.add(new_gateway)
 
     @staticmethod
     def get_by_code(code: str, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         return db.execute(select(models.GateWays).filter_by(code=code)).first()
 class RemoteTerminalUnits:
 
     @staticmethod
     def add(rtu: schemas.RemoteTerminalUnitsCreate, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         new_rtu = models.RemoteTerminalUnits(brand=rtu.brand, model=rtu.model, code=rtu.code, longitude=rtu.longitude,
                                              latitude=rtu.latitude, elevation=rtu.elevation,name=rtu.name, station_id=rtu.station_id)
         db.add(new_rtu)
 
     @staticmethod
     def get_by_code(code: str, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         return db.execute(select(models.RemoteTerminalUnits).filter_by(code = code)).first()
     
     @staticmethod
     def get_by_station_id(station_id: int, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         return db.execure(select(models.RemoteTerminalUnits).filter_by(station_id=station_id)).first() 
 class MonitoredParameters:
 
     @staticmethod
     def add(monitored_parameters: schemas.MonitoredParametersCreate, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         new_monitored_parameters = models.MonitoredParameters(type=monitored_parameters.device_type, measurement=monitored_parameters.measurement, unit=monitored_parameters.unit,
                                                 device_height=monitored_parameters.device_height, name =monitored_parameters.name,
                                                 code=monitored_parameters.code, station_id=monitored_parameters.station_id, rtu_id=monitored_parameters.rtu_id)
@@ -142,9 +154,11 @@ class MeasurementsTranslations:
 
     @staticmethod
     def add(translation: schemas.MeasurementTranslationsCreate, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         new_translation = models.MeasurementTranslations(measurement=translation.measurement, el=translation.el, en=translation.en)
         db.add(new_translation)
     
     @staticmethod
     def get_translation_by_measurement(measurement: str, db: Session = SessionLocal()):
+        event.listen(db, 'before_flush', log_sqlalchemy_session_events)
         return db.execute(select(models.MeasurementTranslations).filter_by(measurement=measurement)).first()
